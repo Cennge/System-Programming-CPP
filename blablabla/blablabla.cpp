d56c3c52e919661d57828981022d672d0b04f8c3
Ilya <ilyagliba02@gmail.com>
2025-02-09 15:09:16 +0200

    added counters

--- blablabla/blablabla.cpp
+++ blablabla/blablabla.cpp
@@ -2,7 +2,6 @@
 #include "blablabla.h"
 
 #define MAX_LOADSTRING 100
-#define RESUME_BUTTON 1
 
 HINSTANCE hInst;
 WCHAR szTitle[MAX_LOADSTRING];
@@ -15,53 +14,13 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int AskUser(int guess) {
-    wchar_t buffer[50];
-    swprintf(buffer, 50, L"Ваше число %d?", guess);
 
-    int result = MessageBox(NULL, buffer, L"Угадай число", MB_YESNOCANCEL | MB_ICONQUESTION);
-    if (result == IDYES) return 0;
-    if (result == IDCANCEL) return -2; 
 
-    result = MessageBox(NULL, L"Ваше число больше?", L"Угадай число", MB_YESNO | MB_ICONQUESTION);
-    if (result == IDYES) return 1;
-
-    return -1;
-}
-
-void PlayGame() {
-    int low = 1, high = 100, attempts = 0;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        int response = AskUser(mid);
-        attempts++;
-
-        if (response == 0) {
-            wchar_t buffer[50];
-            swprintf(buffer, 50, L"Число угадано за %d попыток!", attempts);
-            MessageBox(NULL, buffer, L"Победа!", MB_OK | MB_ICONINFORMATION);
-            break;
-        }
-        else if (response == 1) {
-            low = mid + 1;
-        }
-        else if (response == -1) {
-            high = mid - 1;
-        }
-        else {
-            return; 
-        }
-    }
-
-    int retry = MessageBox(NULL, L"Хотите сыграть еще раз?", L"Новая игра", MB_YESNO | MB_ICONQUESTION);
-    if (retry == IDYES) PlayGame();
-}
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow)
 {
-    PlayGame();
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
@@ -131,46 +90,23 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
     TCHAR str[50];
+    static int counterLBM = 0;
+    static int counterRBM = 0;
 
     switch (uMessage)
     {
-    case WM_CREATE:
-        hButton = CreateWindowW(
-            L"BUTTON",
-            L"Резюме!",
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            50, 50, 150, 40,
-            hWnd,
-            (HMENU)RESUME_BUTTON,
-            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
-            NULL
-        );
-        break;
-
     case WM_COMMAND:
-        if (LOWORD(wParam) == RESUME_BUTTON)
-        {
-            const wchar_t* text1 = L"Написать приложение, позволяющее вывести на экран краткое резюме с помощью последовательности окон сообщений";
-            const wchar_t* text2 = L"(количество окон сообщений – не менее трёх). На заголовке последнего окна сообщения должно отобразиться";
-            const wchar_t* text3 = L"среднее число символов на странице (общее число символов в резюме поделить на количество окон сообщений)";
-
-            double d_title = (wcslen(text1) + wcslen(text2) + wcslen(text3)) / 3.0;
-
-            const int size = 64;
-            wchar_t title[size];
-            swprintf(title, size, L"%.2f", d_title);
-
-            MessageBox(hWnd, text1, L"Резюме", MB_OK | MB_ICONINFORMATION);
-            MessageBox(hWnd, text2, L"Резюме", MB_OK | MB_ICONINFORMATION);
-            MessageBox(hWnd, text3, title, MB_OK | MB_ICONINFORMATION);
-        }
         break;
-
     case WM_LBUTTONDOWN:
-        wsprintf(str, TEXT("Hello world!"));
+        counterLBM++;
+        wsprintf(str, TEXT("LMB: %d | RMB: %d"), counterLBM, counterRBM);
         SetWindowText(hWnd, str);
         break;
-
+    case WM_RBUTTONDOWN:
+        counterRBM++;
+        wsprintf(str, TEXT("LMB: %d | RMB: %d"), counterLBM, counterRBM);
+        SetWindowText(hWnd, str);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
