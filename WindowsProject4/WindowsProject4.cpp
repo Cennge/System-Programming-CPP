2c5d62c49c1c1a7a4b87407eb0509812759226cc
Ilya <ilyagliba02@gmail.com>
2025-02-24 21:10:12 +0200

    completed

--- WindowsProject4/WindowsProject4.cpp
+++ WindowsProject4/WindowsProject4.cpp
@@ -5,7 +5,6 @@
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
-
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {
     srand((unsigned)time(0));
@@ -14,29 +13,57 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLin
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static int numbers[20]; // Массив для хранения сгенерированных чисел
+    static int count = 0;   // Количество чисел
+
     switch (message)
     {
     case WM_COMMAND:
         if (LOWORD(wParam) == IDC_BUTTON1) {
             HWND hList = GetDlgItem(hWnd, IDC_LIST1);
-
             SendMessage(hList, LB_RESETCONTENT, 0, 0);
 
-            int count = 10 + rand() % 11;
-
+            count = 10 + rand() % 11;
             for (int i = 0; i < count; i++) {
-                int number = rand() % 10 + 1;
-                if (rand() % 2) number = -number;
+                numbers[i] = (rand() % 10 + 1) * (rand() % 2 ? 1 : -1);
 
                 TCHAR buffer[10];
-                _stprintf_s(buffer, _T("%d"), number);
-
+                _stprintf_s(buffer, _T("%d"), numbers[i]);
                 SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)buffer);
             }
         }
+
+        if (LOWORD(wParam) == IDC_BUTTON1) {
+            int result = 0;
+            HWND hStatic = GetDlgItem(hWnd, IDC_STATIC2);
+
+            if (IsDlgButtonChecked(hWnd, IDC_RADIO1) == BST_CHECKED) {
+                result = 0;
+                for (int i = 0; i < count; i++) result += numbers[i];
+            }
+            else if (IsDlgButtonChecked(hWnd, IDC_RADIO2) == BST_CHECKED) {
+                result = 1;
+                for (int i = 0; i < count; i++) result *= numbers[i];
+            }
+            else if (IsDlgButtonChecked(hWnd, IDC_RADIO3) == BST_CHECKED) {
+                if (count > 0) {
+                    double avg = 0;
+                    for (int i = 0; i < count; i++) avg += numbers[i];
+                    avg /= count;
+
+                    TCHAR buffer[20];
+                    _stprintf_s(buffer, _T("%.2f"), avg);
+                    SetWindowText(hStatic, buffer);
+                    return TRUE;
+                }
+            }
+
+            TCHAR buffer[20];
+            _stprintf_s(buffer, _T("%d"), result);
+            SetWindowText(hStatic, buffer);
+        }
         return TRUE;
-    case WM_SHOWWINDOW:
-        return TRUE;
+
     case WM_CLOSE:
         EndDialog(hWnd, 0);
         return TRUE;
--- WindowsProject4/resource.h
+++ WindowsProject4/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by WindowsProject4.rc
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется WindowsProject4.rc
 //
 #define IDC_MYICON                      2
 #define IDD_WINDOWSPROJECT4_DIALOG      102
@@ -20,7 +20,8 @@
 #define IDC_RADIO2                      1010
 #define IDC_RADIO3                      1011
 #define IDC_LIST1                       1012
-#define IDC_STATIC                      -1
+#define IDC_STATIC2                     1013
+#define IDC_STATIC                      1
 
 // Next default values for new objects
 // 
@@ -29,7 +30,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
