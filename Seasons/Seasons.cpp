54c9d2a935a0af2fd255a0086a19cf27f5a1b60e
Ilya <ilyagliba02@gmail.com>
2025-02-27 11:35:11 +0200

    Copy completed

--- Seasons/Seasons.cpp
+++ Seasons/Seasons.cpp
@@ -1,42 +1,76 @@
 #include <windows.h>
-#include <tchar.h>
+#include <fstream>
 #include "resource.h"
 
-HWND hList, hCombo, hCheck, hEdit;
+using namespace std;
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+DWORD WINAPI CopyFileThread(LPVOID lpParam);
 
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+struct CopyData {
+    HWND hWnd;
+    char sourcePath[MAX_PATH];
+    char pastePath[MAX_PATH];
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc); 
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
+}
+
+void StartCopying(HWND hWnd)
+{
+    CopyData* data = new CopyData;
+    data->hWnd = hWnd;
+
+    GetDlgItemTextA(hWnd, IDC_EDIT1, data->sourcePath, MAX_PATH);
+    GetDlgItemTextA(hWnd, IDC_EDIT3, data->pastePath, MAX_PATH);
+
+    CreateThread(NULL, 0, CopyFileThread, data, 0, NULL);
+}
+
+DWORD WINAPI CopyFileThread(LPVOID lpParam)
+{
+    CopyData* data = (CopyData*)lpParam;
+
+    ifstream src(data->sourcePath, ios::binary);
+    ofstream paste(data->pastePath, ios::binary);
+
+    if (src && paste) {
+        char ch;
+        while (src.get(ch)) { 
+            Sleep(10);
+            paste.put(ch);      
+        }
+        MessageBoxA(data->hWnd, "Файл скопирован", "Ура", MB_OK | MB_ICONINFORMATION);
+    }
+    else {
+        MessageBoxA(data->hWnd, "Ошибка при открытии файлов :((((", "Ошибка", MB_OK | MB_ICONERROR);
+    }
+
+    delete data;
+    return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch(message)
-	{
-	case WM_CLOSE:
-		EndDialog(hWnd, 0);
-		return TRUE;
+    switch (message)
+    {
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
 
-	case WM_INITDIALOG:
-		hCombo = GetDlgItem(hWnd, IDC_COMBO1);
-		SendMessage(hCombo, CB_RESETCONTENT, 0, 0);
+    case WM_INITDIALOG:
+        SetDlgItemTextA(hWnd, IDC_EDIT1, "C:\\Users\\example.txt");
+        SetDlgItemTextA(hWnd, IDC_EDIT3, "C:\\Users\\copy.txt");
+        return TRUE;
 
-		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)TEXT("Безадри-221"));
-		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)TEXT("Безадри-231"));
-		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)TEXT("Безадри-232"));
-		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)TEXT("Безадри-241"));
-		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)TEXT("Безадри-242"));
-
-		SendMessage(hCombo, CB_SETCURSEL, 0, 0);
-		return TRUE;
-
-	case WM_COMMAND:
-		if (wParam == IDC_BUTTON1) {
-			EndDialog(hWnd, 0);
-			return 1;
-		}
-	}
-	return FALSE;
+    case WM_COMMAND:
+        if (wParam == IDC_BUTTON1) {
+            StartCopying(hWnd);
+            return TRUE;
+        }
+        break;
+    }
+    return FALSE;
 }
--- Seasons/resource.h
+++ Seasons/resource.h
@@ -9,11 +9,15 @@
 #define IDC_EDIT1                       1004
 #define IDC_DAYS                        1004
 #define IDC_LIST1                       1005
+#define IDC_EDIT3                       1005
 #define IDC_BUTTON1                     1006
 #define IDC_STATIC                      1007
 #define IDC_COMBO1                      1007
+#define IDC_BUTTON2                     1007
 #define IDC_RADIO1                      1008
+#define IDC_BUTTON3                     1008
 #define IDC_RADIO2                      1009
+#define IDC_PROGRESS1                   1009
 #define IDC_RADIO3                      1010
 #define IDC_EDIT2                       1011
 
@@ -23,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
