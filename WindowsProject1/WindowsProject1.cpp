c309e9272bb5c42b79b17a157c6feb0b56e1d70d
Ilya <ilyagliba02@gmail.com>
2025-03-02 12:46:18 +0200

    fstream

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -1,10 +1,13 @@
 ﻿#include <windows.h>
 #include <tchar.h>
 #include "resource.h"
+#include <fstream>
 #include <ctime>
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 DWORD WINAPI FilesRandomNum(LPVOID lpParam);
+
+using namespace std;
 
 const TCHAR* fileNames[] = { _T("1.txt"), _T("2.txt"), _T("3.txt") };
 
@@ -13,7 +16,7 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLin
     return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
 }
 
-BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM)
 {
     switch (message)
     {
@@ -22,7 +25,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             EndDialog(hWnd, 0);
         }
         else if (LOWORD(wParam) == IDC_BUTTON2) {
-            srand((unsigned int)time(NULL));
+            srand(static_cast<unsigned int>(time(NULL)));
             for (int i = 0; i < 3; i++) {
                 CreateThread(NULL, 0, FilesRandomNum, (LPVOID)i, 0, NULL);
             }
@@ -38,15 +41,12 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 DWORD WINAPI FilesRandomNum(LPVOID lpParam)
 {
     int fileIndex = (int)lpParam;
-    HANDLE hFile = CreateFile(fileNames[fileIndex], GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile != INVALID_HANDLE_VALUE) {
-        DWORD bytesWritten;
-        TCHAR buffer[20];
+    ofstream file(fileNames[fileIndex], ios::app);//append mode
+    if (file.is_open()) {
         for (int i = 0; i < 10; i++) {
-            wsprintf(buffer, _T("%d\r\n"), rand() % 100);
-            WriteFile(hFile, buffer, lstrlen(buffer) * sizeof(TCHAR), &bytesWritten, NULL);
+            file << rand() % 100 << "\n";
         }
-        CloseHandle(hFile);
     }
     return 0;
 }
+
